d1e786d940e8907a66fc23d9251e74a684a00d0a
K4OS <maxence.perronie@epitech.eu>
2023-02-03 23:12:24 +0100

    desactivation des mouv de bras

--- fightings.lua
+++ fightings.lua
@@ -1,3 +1,16 @@
+Client.OnStart = function()
+
+    -- dropPlayer = function()
+
+	Player.RightArm.IgnoreAnimations = true
+	Player.RightHand.IgnoreAnimations = true
+	Player.LeftArm.IgnoreAnimations = true
+	Player.LeftHand.IgnoreAnimations = true
+
+    --World:AddChild(Player)
+
+end
+
 Client.Action2 = function()
     Player:SwingLeft()
   end
